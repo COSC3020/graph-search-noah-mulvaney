5586d0a38830b5b5c873fa5f2d22a70342bd64fd
Noah Mulvaney <mulvaney.np@gmail.com>
2024-03-01 19:21:11 -0700

    Implement depth first search

--- code.js
+++ code.js
@@ -1,3 +1,45 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+// COSC3020 Graph Search Exercise
+// Noah Mulvaney, nmulvane@uwyo.edu
+// 28 Feb 2024
+
+// Depth First Search of Graph (Adjacency Matrix)
+function depthFirstSearch(graph, start, target) {
+    let visit = []; // nodes to visit stack
+    let path = []; // visited vertices
+    let i = start; // current node
+
+    // really simple fast cases
+    if (graph[start][target] == 1) {
+        if (start != target) return [start, target];
+        else return [start];
+    }
+
+    // find the target, depth first search
+    while (i != target) {
+        path.push(i);
+
+        // add adjacent vertices to stack
+        for (let j = graph[0].length - 1; j >= 0; j--) 
+            if (graph[i][j] == 1 && path.indexOf(j) == -1) visit.push(j);
+
+        // select next vertex
+        i = visit.pop();
+        if (i == undefined) return [];
+    }
+    path.push(target);
+
+    // reduce to get a more direct path
+    let dir_path = [target]; // direct path from start to target
+    for (let j = path.length - 2; j > 0; j--) {
+        if (graph[path[j]][dir_path[0]] == 1) dir_path.unshift(path[j]);
+        if (graph[start][path[j]] != 0) break;
+    }
+    if (start != target) dir_path.unshift(start);
+
+    return(dir_path);
 }
+
+
+for (let i = 0; i < 5; i++)
+    for (let j = 0; j < 5; j++)
+        console.log("start:", i, "end:", j, "Result:", depthFirstSearch([[0,1,1,1,0],[0,0,0,1,0],[0,0,0,0,0],[0,0,1,0,1],[0,1,0,0,0]], i, j));
