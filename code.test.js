975f43cabb0bb111d6cdb260d869eab93f8fb359
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-03-06 16:44:05 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -13,6 +13,7 @@ eval(fs.readFileSync('code.js')+'');
 
 const test =
     jsc.forall("array (pair nat nat)", function(edges) {
+        /**
         // create a random matrix
         var max = edges.reduce(function(a, b) { return Math.max(a, Math.max(b[0], b[1])); }, 0);
         var mat = [];
@@ -43,7 +44,26 @@ const test =
         // check if path is connected
         for (let i = 1; i < path.length; i++)
             if (mat[path[i - 1]][path[i]] != 1) return false;
+        **/
 
+        let matrix = [[0,1,0,1,0,0],[0,0,1,0,0,0],[0,0,0,0,0,1],[0,0,1,0,1,0],[0,0,1,0,0,1],[0,0,0,0,0,0]];
+        if (depthFirstSearch(matrix, 0, 0) != [0]) return false;
+        if (depthFirstSearch(matrix, 0, 1) != [0,1]) return false;
+        if (depthFirstSearch(matrix, 0, 2) != [0,1,2]) return false;
+        if (depthFirstSearch(matrix, 0, 3) != [0,3]) return false;
+        if (depthFirstSearch(matrix, 0, 4) != [0,3,4]) return false;
+        if (depthFirstSearch(matrix, 0, 5) != [0,1,2,5]) return false;
+        if (depthFirstSearch(matrix, 1, 3) != []) return false;
+        if (depthFirstSearch(matrix, 1, 5) != [0,2,5]) return false;
+        if (depthFirstSearch(matrix, 2, 3) != []) return false;
+        if (depthFirstSearch(matrix, 2, 5) != [2,5]) return false;
+        if (depthFirstSearch(matrix, 3, 4) != [3,4]) return false;
+        if (depthFirstSearch(matrix, 3, 5) != [3,2,5) return false;
+        if (depthFirstSearch(matrix, 4, 5) != [4,5) return false;
+        if (depthFirstSearch(matrix, 4, 1) != []) return false;
+        if (depthFirstSearch(matrix, 5, 0) != []) return false;
+        if (depthFirstSearch(matrix, 5, 2) != []) return false;
+        
         return true;
     });
 jsc.assert(test, { tests: 1000 });
