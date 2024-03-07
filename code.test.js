7493471cc82a081ecb08fd5a224c434f20f241ba
Noah Mulvaney <mulvaney.np@gmail.com>
2024-03-06 19:55:01 -0700

    Update tests

--- code.test.js
+++ code.test.js
@@ -14,22 +14,22 @@ eval(fs.readFileSync('code.js')+'');
 const test = function () {
 
         let matrix = [[0,1,0,1,0,0],[0,0,1,0,0,0],[0,0,0,0,0,1],[0,0,1,0,1,0],[0,0,1,0,0,1],[0,0,0,0,0,0]];
-        if (depthFirstSearch(matrix, 0, 0) != [0]) return false;
-        if (depthFirstSearch(matrix, 0, 1) != [0,1]) return false;
-        if (depthFirstSearch(matrix, 0, 2) != [0,1,2]) return false;
-        if (depthFirstSearch(matrix, 0, 3) != [0,3]) return false;
-        if (depthFirstSearch(matrix, 0, 4) != [0,3,4]) return false;
-        if (depthFirstSearch(matrix, 0, 5) != [0,1,2,5]) return false;
-        if (depthFirstSearch(matrix, 1, 3) != []) return false;
-        if (depthFirstSearch(matrix, 1, 5) != [0,2,5]) return false;
-        if (depthFirstSearch(matrix, 2, 3) != []) return false;
-        if (depthFirstSearch(matrix, 2, 5) != [2,5]) return false;
-        if (depthFirstSearch(matrix, 3, 4) != [3,4]) return false;
-        if (depthFirstSearch(matrix, 3, 5) != [3,2,5]) return false;
-        if (depthFirstSearch(matrix, 4, 5) != [4,5]) return false;
-        if (depthFirstSearch(matrix, 4, 1) != []) return false;
-        if (depthFirstSearch(matrix, 5, 0) != []) return false;
-        if (depthFirstSearch(matrix, 5, 2) != []) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 0)) != JSON.stringify([0])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 1)) != JSON.stringify([0,1])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 2)) != JSON.stringify([0,1,2])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 3)) != JSON.stringify([0,3])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 4)) != JSON.stringify([0,3,4])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 0, 5)) != JSON.stringify([0,1,2,5])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 1, 3)) != JSON.stringify([])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 1, 5)) != JSON.stringify([0,2,5])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 2, 3)) != JSON.stringify([])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 2, 5)) != JSON.stringify([2,5])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 3, 4)) != JSON.stringify([3,4])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 3, 5)) != JSON.stringify([3,2,5])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 4, 5)) != JSON.stringify([4,5])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 4, 1)) != JSON.stringify([])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 5, 0)) != JSON.stringify([])) return false;
+        if (JSON.stringify(depthFirstSearch(matrix, 5, 2)) != JSON.stringify([])) return false;
         
         return true;
     };
